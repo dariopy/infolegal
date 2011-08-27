1f68d582f76ebafcade6776e75d209920ef7b3f6
Dario Alvarez <dariopy@gmail.com>
2011-08-27 01:59:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infolegal::Application.config.secret_token = '23d81d79ecc508189b54f0ad0f1f83a05f74c03db41596b446dde0b71b7b9a2fb2635e19d2693411982dfa0801286c31d902c6ee7a704afce2379214efaaa98e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Infolegal::Application.config.session_store :cookie_store, :key => '_infolegal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Infolegal::Application.config.session_store :active_record_store
